fc83bccd57f6d85ede86f71e29d9bd6d5738efc0
Mani P <mani.p2@dbtlabscoalesce.com>
2024-10-01 18:28:55 +0000

    added contract and versioning

--- models/marts/fct_orders_v2.sql
+++ models/marts/fct_orders_v2.sql
@@ -8,14 +8,14 @@ final as (
         location_id,
         customer_id,
         order_total,
+        case when order_total > 100 then true else false end as high_order_value,
         tax_paid,
         ordered_at,
         customer_name,
         location_name,
-        tax_rate,
         location_opened_at,
         date_part(month, ordered_at) as ordered_month,
-        date_part(day, ordered_at) as ordered_day, 
+        date_part(day, ordered_at) as ordered_day,
         date_part(year, ordered_at) as ordered_year
     from orders
 )
