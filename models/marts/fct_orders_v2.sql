d4a627fe273f699b4779cb15667061fc3e5017ac
Christopher Alderson <christopher.alderson@dbtlabscoalesce.com>
2024-10-07 23:41:57 +0000

    add model versions

---
+++ models/marts/fct_orders_v2.sql
@@ -0,0 +1,24 @@
+with orders as (
+    select * from {{ ref('int_orders') }}
+),
+
+final as (
+    select 
+        order_id,
+        location_id,
+        customer_id,
+        order_total,
+        tax_paid,
+        ordered_at,
+        customer_name,
+        location_name,
+        case when order_total > 100 then true else false end as high_order_value,
+        location_opened_at,
+        date_part(month, ordered_at) as ordered_month,
+        date_part(day, ordered_at) as ordered_day, 
+        date_part(year, ordered_at) as ordered_year
+    from orders
+)
+
+select * 
+from final
